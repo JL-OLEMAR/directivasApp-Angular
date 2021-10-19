d30080415b53d4556306ada488b4a7d11defab42
JL-OLEMAR <joseluis19963@gmail.com>
2021-10-18 23:12:35 -0500

    Estructura de la aplicacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'productos',
+    loadChildren: async () => await import('./productos/productos.module').then(m => m.ProductosModule)
+  },
+  { path: '**', redirectTo: 'productos' }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
